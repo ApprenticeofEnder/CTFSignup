de2aef49f2958a4f4f884d3ab5ba869c7e1a20d9
ApprenticeofEnder <perfknight@gmail.com>
2022-01-30 20:01:45 -0500

    update settings file

--- raven_signup/settings.py
+++ raven_signup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("APP_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.carletoncybersec.club']
 
 
 # Application definition
